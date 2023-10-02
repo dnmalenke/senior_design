e1c1b66499456fc9d926f6d83fdfef08269b17c9
David Malenke <dnmalenke@gmail.com>
2023-10-02 16:51:20 -0500

    add display code

--- vehicle/main.py
+++ vehicle/main.py
@@ -6,6 +6,10 @@ import asyncio
 import pickle
 import arucodict
 import displayio
+import busio
+import terminalio
+
+from adafruit_display_text import label
 from adafruit_st7789 import ST7789
 
 from controlpacket import *
@@ -16,13 +20,48 @@ WIFI_PASSWORD = "password"
 UDP_PORT = 1234
 
 class Main:
-    def __init__(self):
-        pass
-
     def init_hardware(self):
         self.led = digitalio.DigitalInOut(board.IO21)
         self.led.direction = digitalio.Direction.OUTPUT
+
         # https://docs.circuitpython.org/projects/st7789/en/latest/index.html
+
+        displayio.release_displays()
+
+        spi = busio.SPI(board.IO8, board.IO10, board.IO9)
+        while not spi.try_lock():
+            pass        
+        spi.configure(baudrate=24000000) # Configure SPI for 24MHz
+        spi.unlock()
+
+        display_bus = displayio.FourWire(spi, command=board.IO6, chip_select=board.IO7, reset=board.IO5) # check if we don't need reset and check if we can go without command pin as well
+
+        display = ST7789(display_bus, width=280, height=240, rowstart=20, rotation=90)
+        splash = displayio.Group()
+        display.show(splash)
+
+        color_bitmap = displayio.Bitmap(280, 240, 1)
+        color_palette = displayio.Palette(1)
+        color_palette[0] = 0x00FF00  # Bright Green
+
+        bg_sprite = displayio.TileGrid(color_bitmap, pixel_shader=color_palette, x=0, y=0)
+        splash.append(bg_sprite)
+
+        # Draw a smaller inner rectangle
+        inner_bitmap = displayio.Bitmap(240, 200, 1)
+        inner_palette = displayio.Palette(1)
+        inner_palette[0] = 0xAA0088  # Purple
+        inner_sprite = displayio.TileGrid(inner_bitmap, pixel_shader=inner_palette, x=20, y=20)
+        splash.append(inner_sprite)
+
+        # Draw a label
+        text_group = displayio.Group(scale=3, x=37, y=120)
+        text = "Hello World!"
+        text_area = label.Label(terminalio.FONT, text=text, color=0xFFFF00)
+        text_group.append(text_area)  # Subgroup for text scaling
+        splash.append(text_group)
+        
+
         pass
 
     # David
@@ -37,6 +76,7 @@ class Main:
 
     # Jon
     def handle_packet(self, packet: ControlPacket):
+
         pass
 
     # David
@@ -48,8 +88,6 @@ class Main:
             return
         
         data = arucodict.aruco_dict[id]
-
-
 
     async def receive_task(self):
         if self.sock is None:
@@ -74,6 +112,7 @@ class Main:
     async def main(self):
         heartbeat_task = asyncio.create_task(self.heartbeat_task())
 
+        self.init_hardware()
         self.configure_wifi()
         self.display_tag()
         
