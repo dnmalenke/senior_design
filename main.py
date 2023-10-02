96fa38cc9477daf27c881286e2ab0a06355c909d
David Malenke <dnmalenke@gmail.com>
2023-10-02 10:42:43 -0500

    add networking code

--- main.py
+++ main.py
@@ -1,10 +1,19 @@
-import time
 import board
 import digitalio
 import wifi
 import socketpool
+import asyncio
+import pickle
+import arucodict
+import displayio
+from adafruit_st7789 import ST7789
 
 from controlpacket import *
+
+WIFI_SSID = "SDES"
+WIFI_PASSWORD = "password"
+
+UDP_PORT = 1234
 
 class Main:
     def __init__(self):
@@ -13,13 +22,18 @@ class Main:
     def init_hardware(self):
         self.led = digitalio.DigitalInOut(board.IO21)
         self.led.direction = digitalio.Direction.OUTPUT
+        # https://docs.circuitpython.org/projects/st7789/en/latest/index.html
         pass
 
     # David
     def configure_wifi(self):
-        # https://github.com/adafruit/circuitpython/blob/main/tests/circuitpython-manual/socketpool/server/cpy-server.py
-        # asyncio for packet waiting/handling
-        pass
+        wifi.radio.connect(WIFI_SSID,WIFI_PASSWORD)
+        pool = socketpool.SocketPool(wifi.radio)
+        
+        udp_host = str(wifi.radio.ipv4_address)
+
+        self.sock = pool.socket(pool.AF_INET, pool.SOCK_DGRAM)
+        self.sock.bind((udp_host, UDP_PORT))
 
     # Jon
     def handle_packet(self, packet: ControlPacket):
@@ -27,16 +41,47 @@ class Main:
 
     # David
     def display_tag(self):
-        pass
+        id = int(str(wifi.radio.ipv4_address).split('.')[3])
 
-    def main(self):
+        if id < 0 or id > 249:
+            print('ip address out of range')
+            return
+        
+        data = arucodict.aruco_dict[id]
+
+
+
+    async def receive_task(self):
+        if self.sock is None:
+            return
+        
+        udp_buffer = bytearray(512)
+
+        while True:
+           self.sock.recvfrom_into(udp_buffer)           
+           packet = pickle.loads(udp_buffer)
+           if packet is ControlPacket:
+                self.handle_packet(packet)
+
+    async def heartbeat_task(self):
         while True:
             self.led.value = True
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
             self.led.value = False
-            time.sleep(0.5)
+            await asyncio.sleep(0.5)
+            
 
+    async def main(self):
+        heartbeat_task = asyncio.create_task(self.heartbeat_task())
+
+        self.configure_wifi()
+        self.display_tag()
+        
+        receive_task = asyncio.create_task(self.receive_task())
+
+        # don't await heartbeat_task because we want it to stop if receive task fails
+        await asyncio.wait([receive_task])
 
 if __name__ == "__main__":
-    Main().main()
+    asyncio.run(Main().main())
         
