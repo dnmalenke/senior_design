f4b50a724563fe0ec55be7d21aecbfd93452dd06
David Malenke <dnmalenke@gmail.com>
2023-11-13 17:20:24 -0600

    lots of ui code needs cleanup eventually

--- dubins_path.py
+++ dubins_path.py
@@ -17,7 +17,7 @@ def plan_dubins_path(s_x, s_y, s_yaw, g_x, g_y, g_yaw, curvature, tolerance = 75
                      step_size=0.1, selected_types=None):
  
     if sqrt((g_x-s_x)**2 + (g_y-s_y)**2) < tolerance:
-        return [0], [0], [0], ["STOP"], [("STOP",100),("STOP",100),("STOP",100)]
+        return [("STOP",0),("STOP",0),("STOP",0)]
      
     if selected_types is None:
         planning_funcs = _PATH_TYPE_MAP.values()
@@ -31,18 +31,10 @@ def plan_dubins_path(s_x, s_y, s_yaw, g_x, g_y, g_yaw, curvature, tolerance = 75
     local_goal_y = le_xy[1]
     local_goal_yaw = g_yaw - s_yaw
 
-    lp_x, lp_y, lp_yaw, modes, lengths = _dubins_path_planning_from_origin(
-        local_goal_x, local_goal_y, local_goal_yaw, curvature, step_size,
-        planning_funcs)
+    modes, lengths = _dubins_path_planning_from_origin(local_goal_x, local_goal_y, local_goal_yaw, curvature, step_size,planning_funcs)
     vectors = [(modes[0], lengths[0]), (modes[1], lengths[1]), (modes[2], lengths[2])]
-    # Convert a local coordinate path to the global coordinate
-    rot = rot_mat_2d(-s_yaw)
-    converted_xy = np.stack([lp_x, lp_y]).T @ rot
-    x_list = converted_xy[:, 0] + s_x
-    y_list = converted_xy[:, 1] + s_y
-    yaw_list = angle_mod(np.array(lp_yaw) + s_yaw)
 
-    return x_list, y_list, yaw_list, modes, vectors 
+    return vectors 
 
 
 def _mod2pi(theta):
@@ -164,12 +156,10 @@ def _dubins_path_planning_from_origin(end_x, end_y, end_yaw, curvature,
             b_d1, b_d2, b_d3, b_mode, best_cost = d1, d2, d3, mode, cost
 
     lengths = [b_d1, b_d2, b_d3]
-    x_list, y_list, yaw_list = _generate_local_course(lengths, b_mode,
-                                                      curvature, step_size)
     
     lengths = [length / curvature for length in lengths]
 
-    return x_list, y_list, yaw_list, b_mode, lengths
+    return b_mode, lengths
 
 
 def _interpolate(length, mode, max_curvature, origin_x, origin_y,
@@ -196,30 +186,6 @@ def _interpolate(length, mode, max_curvature, origin_x, origin_y,
             path_yaw.append(origin_yaw - length)
 
     return path_x, path_y, path_yaw
-
-
-def _generate_local_course(lengths, modes, max_curvature, step_size):
-    p_x, p_y, p_yaw = [0.0], [0.0], [0.0]
-
-    for (mode, length) in zip(modes, lengths):
-        if length == 0.0:
-            continue
-
-        # set origin state
-        origin_x, origin_y, origin_yaw = p_x[-1], p_y[-1], p_yaw[-1]
-
-        current_length = step_size
-        while abs(current_length + step_size) <= abs(length):
-            p_x, p_y, p_yaw = _interpolate(current_length, mode, max_curvature,
-                                           origin_x, origin_y, origin_yaw,
-                                           p_x, p_y, p_yaw)
-            current_length += step_size
-
-        p_x, p_y, p_yaw = _interpolate(length, mode, max_curvature, origin_x,
-                                       origin_y, origin_yaw, p_x, p_y, p_yaw)
-
-    return p_x, p_y, p_yaw
-
 
 def main():
     import matplotlib.pyplot as plt
--- server.py
+++ server.py
@@ -3,6 +3,7 @@ import cv2
 import sys
 import socket
 import json
+import ui_helpers
 from controlpacket import *
 from dubins_path import plan_dubins_path
 
@@ -39,13 +40,19 @@ class Main():
         self.cap.set(cv2.CAP_PROP_GAIN, 1.0)
 
     def init_aruco(self):
+        self.markers = {}
         aruco_dict = cv2.aruco.getPredefinedDictionary(cv2.aruco.DICT_4X4_250)
 
         self.aruco_detector = cv2.aruco.ArucoDetector(aruco_dict)
 
     def init_gui(self):
-        cv2.setMouseCallback("frame", self.handle_mouse)
-
+        cv2.namedWindow('frame')
+        cv2.setMouseCallback('frame', self.handle_mouse)
+        self.selected_box = -1
+        self.selected_arrow = -1
+        self.selected_point = -1
+        self.destinations = {}
+        
     # David
     def send_packet(self, id, left_speed, right_speed):
         packet = ControlPacket()
@@ -53,7 +60,7 @@ class Main():
         packet.right_speed = right_speed
 
         jd = json.dumps(packet, default=vars)
-        print(jd)
+        # print(jd)
         data = jd.encode()
         try:
             self.socket.sendto(data,(f"{self.ip_start}.{id}", UDP_PORT))
@@ -61,92 +68,198 @@ class Main():
             print(e)
             pass
 
-    def handle_tag(self, corners, id, frame):
-        x1 = corners.item(0)
-        y1 = corners.item(1)
+    def draw_path(self, frame, start_x, start_y, start_yaw, end_x,end_y,end_yaw,vectors, curvature,id):
+        x1, y1, a1 = start_x, start_y, start_yaw
+        radius = 1 / curvature
 
-        x2 = corners.item(2)
-        y2 = corners.item(3)
-    
-        x3 = corners.item(4)
-        y3 = corners.item(5)
-    
-        x4 = corners.item(6)
-        y4 = corners.item(7)
+        for dir, length in vectors:
+            if dir == 'S':
+                x2 = length * np.cos(a1) + x1
+                y2 = length * np.sin(a1) + y1
+
+                cv2.line(frame, (int(x1), int(y1)), (int(x2), int(y2)), (255, 0, 0), 2)
+
+                x1, y1 = x2, y2
+
+            else:
+                if dir == 'R':
+                    start_angle = a1 - np.pi/2
+                    base_angle = 90
+                    end_angle = -length / radius
+                elif dir == 'L':
+                    start_angle = a1 + np.pi/2
+                    base_angle = -90
+                    end_angle = length / radius
+                else:
+                    continue
+
+                x_center = x1 + radius * np.cos(start_angle)
+                y_center = y1 + radius * np.sin(start_angle)
+
+                cv2.ellipse(frame, (int(x_center), int(y_center)), (int(radius), int(radius)),
+                            np.rad2deg(a1) + base_angle, 0, np.rad2deg(end_angle), (255, 0, 0), 2)
+
+                x1 = x_center + radius * np.cos(end_angle + a1 + np.deg2rad(base_angle))
+                y1 = y_center + radius * np.sin(end_angle + a1 + np.deg2rad(base_angle))
+
+                a1 = end_angle + a1
+
+        arrlen = 75
+        arrowpoint = (int(arrlen * np.cos(end_yaw) + end_x), int(arrlen * np.sin(end_yaw) + end_y))
+
+        if id == self.selected_arrow:
+            arrow_color = (255,0,255)
+        else:
+            arrow_color = (255,255,0)
+
+        if id == self.selected_point:
+            point_color = (255,0,255)
+        else:
+            point_color = (0,255,0)
+
+        cv2.arrowedLine(frame, (int(end_x), int(end_y)), arrowpoint, arrow_color, 3)
+        cv2.drawMarker(frame, (int(end_x),int(end_y)),point_color,cv2.MARKER_CROSS,10,3)        
+
+    def handle_tag(self, corners, id, frame):
+        if id not in self.destinations:
+            return
+        
+        end_x,end_y, end_yaw = self.destinations[id]
+        x1,y1 = corners[0]
+        x2,y2 = corners[1]
+        x3,y3 = corners[2]
+        x4,y4 = corners[3]
 
         cx = ((x1 + x2 + x4) / 3 + (x2 + x3 + x4) / 3) / 2
         cy = ((y1 + y2 + y4) / 3 + (y2 + y3 + y4) / 3) / 2
 
         centerpoint = (int(cx), int(cy))
 
-        angle = np.arctan2((y2 - y3), (x2 - x3))
-        angle += np.pi/2
-        arrlen = 75
-        arrowpoint = (int(arrlen * np.cos(angle) + cx), int(arrlen * np.sin(angle) + cy))
-
-        cv2.arrowedLine(frame, centerpoint, arrowpoint, (255, 0, 255), 5)
-        cv2.putText(frame, f"location: {centerpoint}. angle: {np.round(np.rad2deg(angle),2)} deg", (centerpoint[0], centerpoint[1] + 20), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)
+        angle = np.arctan2((y2 - y3), (x2 - x3)) + np.pi/2
 
         start_x = float(centerpoint[0]) # [m]
         start_y = float(centerpoint[1]) # [m]
         start_yaw = angle  # [rad]
 
-        end_x = 300.0  # [m]
-        end_y = 300.0  # [m]
-        end_yaw = np.deg2rad(180)  # [rad]
-
-        cv2.drawMarker(frame, (int(end_x),int(end_y)),(0, 255, 0),cv2.MARKER_CROSS,10,3)
-
         curvature = 0.008
-        #x_list, y_list, yaw_list, b_d1, b_d2, b_d3, b_mode, lengths
-        path_x, path_y, path_yaw, mode, vectors = plan_dubins_path(start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature)
-        print(vectors)
+        
+        vectors = plan_dubins_path(start_x, start_y, start_yaw, end_x, end_y, end_yaw, curvature)
 
-        for x,y in zip(path_x,path_y):
-            cv2.drawMarker(frame, (int(x),int(y)),(255, 0, 0),cv2.MARKER_SQUARE,2,1)
+        self.draw_path(frame,start_x,start_y,start_yaw,end_x,end_y,end_yaw,vectors,curvature,id)
 
         for dir,len in vectors:
             if len < 30:
                 continue
             match dir:
                 case 'L':
-                    self.send_packet(id,0.05,-0.3)
+                    self.send_packet(id,0.05,0.3)
                 case 'S':
-                    self.send_packet(id,0.2,-0.2)
+                    self.send_packet(id,0.2,0.2)
                 case 'R':
                     self.send_packet(id,0.3,0.05)
                 case 'STOP':
                     self.send_packet(id,0,0)
             break
-        pass
-
-    # Jax
-    def handle_mouse(self, event, x, y, flags, param):
         
-        pass
+    def draw_tag(self, id,corners, frame):
+        x1,y1 = corners[0]
+        x2,y2 = corners[1]
+        x3,y3 = corners[2]
+        x4,y4 = corners[3]
+
+        if id == self.selected_box:
+            color = (0, 255, 0)
+        else:
+            color = (0,0,255)
+
+        cv2.line(frame,(int(x1),int(y1)),(int(x2),int(y2)),color,1)
+        cv2.line(frame,(int(x2),int(y2)),(int(x3),int(y3)),color,1)
+        cv2.line(frame,(int(x3),int(y3)),(int(x4),int(y4)),color,1)
+        cv2.line(frame,(int(x4),int(y4)),(int(x1),int(y1)),color,1)
+
+        cx = ((x1 + x2 + x4) / 3 + (x2 + x3 + x4) / 3) / 2
+        cy = ((y1 + y2 + y4) / 3 + (y2 + y3 + y4) / 3) / 2
+
+        centerpoint = (int(cx), int(cy))
+
+        angle = np.arctan2((y2 - y3), (x2 - x3)) + np.pi/2
+
+        arrlen = 75
+        arrowpoint = (int(arrlen * np.cos(angle) + cx), int(arrlen * np.sin(angle) + cy))
+
+        cv2.arrowedLine(frame, centerpoint, arrowpoint,(255,0,255), 1)
+        cv2.putText(frame, f"id: {id}. location: {centerpoint}. angle: {np.round(np.rad2deg(angle),2)} deg", (centerpoint[0], centerpoint[1] + 20), cv2.FONT_HERSHEY_PLAIN, 1, (0,0,255), 2)    
+
+    def handle_mouse(self, event, x, y, flags, param):
+        if event == cv2.EVENT_LBUTTONDOWN:              
+            arrlen = 75
+            arrwid = 8
+            for id,(end_x,end_y,end_yaw) in self.destinations.items():
+                if(np.sqrt((x-end_x)**2 + (y-end_y)**2) < 30):
+                    self.selected_point = id
+                else:
+                    perp_yaw = end_yaw + np.pi/2
+                    c2 = (arrwid/2 * np.cos(perp_yaw) + end_x, arrwid/2 * np.sin(perp_yaw)+ end_y)
+                    c3 = (-arrwid/2 * np.cos(perp_yaw) + end_x, -arrwid/2 * np.sin(perp_yaw) + end_y)
+                    arr_x = arrlen * np.cos(end_yaw) + end_x
+                    arr_y = arrlen * np.sin(end_yaw) + end_y 
+                    c1 = (arrwid/2 * np.cos(perp_yaw) + arr_x, arrwid/2 * np.sin(perp_yaw)+ arr_y)
+                    c4 = (-arrwid/2 * np.cos(perp_yaw) + arr_x, -arrwid/2 * np.sin(perp_yaw) + arr_y)
+
+                    if(ui_helpers.is_point_inside_quadrilateral((x,y),[c1, c2, c3, c4])):
+                        self.selected_arrow = id  
+
+        if event == cv2.EVENT_MOUSEMOVE:
+            if self.selected_arrow != -1 and self.selected_arrow in self.destinations: 
+                end_x, end_y, _ = self.destinations[self.selected_arrow]
+                end_yaw = np.arctan2((y-end_y),(x-end_x))
+                self.destinations[self.selected_arrow] = (end_x,end_y,end_yaw)
+            if self.selected_point != -1 and self.selected_point in self.destinations:
+                _,_,end_yaw = self.destinations[self.selected_point]
+                self.destinations[self.selected_point] = (x,y,end_yaw)
+
+        if event == cv2.EVENT_LBUTTONUP:
+            self.selected_arrow = -1
+            self.selected_point = -1
+            if(self.selected_box != -1 and self.selected_box in self.markers):
+                corners = self.markers[self.selected_box]
+                if not ui_helpers.is_point_inside_quadrilateral((x,y),corners):
+                    x1,y1 = corners[0]
+                    x2,y2 = corners[1]
+                    x3,y3 = corners[2]
+                    x4,y4 = corners[3]
+
+                    cx = ((x1 + x2 + x4) / 3 + (x2 + x3 + x4) / 3) / 2
+                    cy = ((y1 + y2 + y4) / 3 + (y2 + y3 + y4) / 3) / 2
+
+                    self.destinations[self.selected_box] = (x,y,np.arctan2((y-cy), (x-cx)) )
+                elif self.selected_box in self.destinations:
+                    del(self.destinations[self.selected_box])
+                self.selected_box = -1
+                return
+            
+            for id,corners in self.markers.items():
+                if(ui_helpers.is_point_inside_quadrilateral((x,y),corners)):
+                    self.selected_box = id
 
     def main(self):
         self.init_network()        
         self.init_camera()
         self.init_aruco()
-        # self.init_gui
-
-        
+        self.init_gui()
 
         while True:
             ret, frame = self.cap.read()
 
             gray_frame = cv2.cvtColor(frame, cv2.COLOR_BGR2GRAY)
 
-            corners, ids, _ = self.aruco_detector.detectMarkers(gray_frame)
-            cv2.aruco.drawDetectedMarkers(frame,corners,ids)
-            
+            corners,ids, _ = self.aruco_detector.detectMarkers(gray_frame)
 
             if np.all(ids != None):
-                for i in range(ids.size):
-                    self.handle_tag(corners[i], int(ids[i]), frame)
-
-            
+                for id,corners in zip(ids,corners):
+                    self.markers[int(id[0])] = corners[0]
+                    self.handle_tag(corners[0], int(id[0]), frame)
+                    self.draw_tag(int(id[0]),corners[0], frame)
             
             cv2.imshow('frame', frame)
             if cv2.waitKey(1) & 0xFF == ord('q'):
